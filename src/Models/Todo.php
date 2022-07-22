aba7f0f7feb0c46036699fed2f24e7903fce2279
Jaswinder Singh <sjeswinder@jstrainer.com>
2022-07-22 11:30:33 +0530

    Added model and table for Todos

---
+++ src/Models/Todo.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Demoapp\Todoapp\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Todo extends Model
+{
+    use HasFactory;
+}
--- src/TodoAppServiceProvider.php
+++ src/TodoAppServiceProvider.php
@@ -14,5 +14,14 @@ class TodoAppServiceProvider extends ServiceProvider
         // add route from your packages
         $this->loadRoutesFrom(__DIR__.'/routes/web.php');
         $this->loadViewsFrom(__DIR__.'/resources/views', 'todoapp');
+
+        if($this->app->runningInConsole()) {
+            if(!class_exists('CreateTableTodos')) {
+                $this->publishes([
+                   __DIR__.'/../database/migrations/2022_07_22_054422_create_table_todos.php' => 
+                    database_path('migrations/'. date('Y_m_d_His',time()). 'create_table_todos.php'),
+                ],'migrations')
+            }
+        }
     }
 }
