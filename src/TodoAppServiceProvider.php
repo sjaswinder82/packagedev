281f202229b7eefd518efff8271c2312cfaf48d6
Jaswinder Singh <sjeswinder@jstrainer.com>
2022-07-22 11:31:50 +0530

    fixes

--- src/TodoAppServiceProvider.php
+++ src/TodoAppServiceProvider.php
@@ -20,7 +20,7 @@ class TodoAppServiceProvider extends ServiceProvider
                 $this->publishes([
                    __DIR__.'/../database/migrations/2022_07_22_054422_create_table_todos.php' => 
                     database_path('migrations/'. date('Y_m_d_His',time()). 'create_table_todos.php'),
-                ],'migrations')
+                ],'migrations');
             }
         }
     }
